bdced8b95e70eac589d40bbd9ddb6a3219a5e19c
khein21501 <khein21501@gmail.com>
2022-02-03 05:16:53 +0630

    khh profo

---
+++ khh/project1/pages/khh.js
@@ -0,0 +1,10 @@
+function closeNav() {
+    document.querySelector("#nav-bar").style.width = "0px";
+    document.querySelector("#nav-bar").style.opacity = "20%";
+
+}
+function openNav() {    
+    document.querySelector("#nav-bar").style.width="300px";
+    document.querySelector("#nav-bar").style.opacity = "100%";
+
+}
