44e5fefa70c0e977477101ab008b8cd34d6b2104
khein21501 <khein21501@gmail.com>
2022-03-02 22:51:34 +0630

    ass3 modify

---
+++ z2p7/C++/assFunction/assThree.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+string itemList[5][3];
+void initializeItemData(){
+
+    for (int  i = 0; i < 5; i++)
+    {
+        if (i==0)
+        {
+            cout<<"------Enter Item Date ------"<<endl;
+        }else{
+            cout<<"------Enter Next Item Date ------"<<endl;
+        }
+        
+        for (int  j = 0; j < 3; j++)
+        {
+            if (j==0)
+            {
+                cout<<"Enter Bar Code :";
+                cin>>itemList[i][j];
+            }else if (j==1)
+            {
+                cout<<"Enter Item Name :";
+                cin>>itemList[i][j];
+            }else{
+                cout<<"Enter Price :";
+                cin>>itemList[i][j];
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
+void showData(){
+    cout<<"BarCode\t\tItem\tPrice"<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<itemList[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    
+}
+void searchItemByCode(string barCode){
+    
+    for (int i = 0; i < 5; i++)
+    {
+        if (barCode == itemList[i][3])
+        {
+           for (int j = 0; j < 3; j++)
+           {
+                cout<<itemList[i][j]<<"\t";
+           }
+           
+        
+        }
+    }
+    
+    
+}
+
+int main(){
+    string barCode;
+    initializeItemData();
+    showData();
+    cout<<"Type Bar Code"<<endl;
+    cin>>barCode;
+    searchItemByCode(barCode);
+    return 0;
+}
