3aa6ebef22bd60e708b8ab5033d0d8f8c43f93de
khein21501 <khein21501@gmail.com>
2022-03-29 11:27:43 +0630

    file path change

--- z2p7/C++/emp.cpp
+++ z2p7/C++/emp.cpp
@@ -10,49 +10,80 @@ private:
 public:
     emp(){
 
-    };
-    emp(int id,string name,string postion,int salary){
-        this ->id = id;
+    }
+    emp(int id,string name, string position,int salary){
+        this ->id = id ;
         this ->name = name;
-        this ->postion = postion;
-        this -> salary = salary;
+        this ->position = position;
+        this ->salary = salary;
     }
-    void getId(int id){
+    void setId(int id){
         this ->id = id;
     }
-    int setId(){
+    int getId(){
         return this->id = id;
     }
-    void getName(string name){
+    void setName(string name){
         this -> name = name;
     }
-    string setName(){
+    string getName(){
         return this ->name=name;
     }
-    void getPos(){
-        this -> position = position;
+    void setPos(string position){
+        this->position = position;
     }
-    string setPos(){
+    string getPos(){
         return this -> position = position;
     }
-    void setSal(){
+    void setSalary(int salary){
         this-> salary = salary;
     }
-    int getSal(){
+    int getSalary(){
         return this->salary =salary;
     }
     void displayInfo(){
-        cout<<getId()<<"\t"<<getName()<<"\t"<<getPos()<<"\t"<<getSal(); 
+        cout<<getId()<<"\t"<<getName()<<"\t"<<getPos()<<"\t"<<getSalary()<<endl; 
     }
 };
 
 
 
 int main(){
-    for (int i = 0; i < count; i++)
-    {
-        /* code */
+    emp empA[0];
+    int id,salary;
+    string name,position;
+    
+    int i = 0;
+    while (true){
+        
+        cout<<"Enter Emoloyee"<<i +1<<"information"<<endl;
+        cout<<"Enter Id Number : ";
+        cin>>id;
+        cout<<"Enter name :";
+        cin>>name;
+        cout<<"Enter Position :";
+        cin>>position;
+        cout<<"Enter Salary";
+        cin>>salary;
+        empA[i].setId(id);
+        empA[i].setName(name);
+        empA[i].setPos(position);
+        empA[i].setSalary(salary);
+        if (empA[i].setId(id)== "end" || empA[i].setName(name)=="end" || empA[i].setPos(position)=="end"||empA[i].setSalary(salary)=="end")
+        {
+            break;
+        }
+        
     }
+    
+    cout<<"------Emplyoee Information------"<<endl;
+    for (int  i = 0; i < 5; i++)
+    {
+        empA[i].displayInfo();
+    }
+    
+    
+
     
     return 0;
 }
