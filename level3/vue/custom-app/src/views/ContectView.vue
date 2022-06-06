9c261539c98b9cc0871896748812643f5c31b181
khein21501 <khein21501@gmail.com>
2022-06-06 22:04:32 +0630

    vue single page app start

---
+++ level3/vue/custom-app/src/views/ContectView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Contact page</h2>
+        <Partfile></PartFile>
+    </div>
+</template>
+<script>
+    import PartFile from '@/components/PartFile.vue'
+
+    export default {
+        name: 'AboutView',
+        components:{
+            PartFile,
+        }
+    }
+</script>
