875f65a1d0da72b9d8d13587ead4c9d65fdb70ff
khein21501 <khein21501@gmail.com>
2022-06-13 21:31:47 +0630

    data structure and algo

--- level3/vue/custom-app/src/router/index.js
+++ level3/vue/custom-app/src/router/index.js
@@ -1,46 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router' 
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ContectView from "../views/ContectView.vue"
+import ContactView from '../views/ContactView.vue'
 import ShopView from "../views/ShopView.vue"
-import ShopView from "../views/ShopDetail.vue"
+import ShopDetail from "../views/ShopDetail.vue"
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: ContectView
-  },
-  {
-    path:'/shop',
-    name: 'shop',
-    component:ShopView
-  },
-  {
-    path: '/detail/:id',
-    name: 'shopdetail',
-    component: ShopDetail
-
-  }
-
-
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: '/contact',
+        name: 'contact',
+        component: ContactView
+    },
+    {
+        path: '/shop',
+        name: 'shop',
+        component: ShopView
+    },
+    {
+        path: '/shopdetail/:id',
+        name: 'shopdetail',
+        component: ShopDetail
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
